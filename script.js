4b160288df5639b46c4f3e8e351fda398308999e
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-05-15 16:16:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const parseCode = (str) => {
   // your code here
+	let newStr = split(' ');
+	
+	return {firstName : `${newStr[0]}`, lastName: `${newStr[1]}`,id : `${newStr[2]}`};
 };
 
 // Do not change the code below
 const str = prompt("Enter str: ");
+
 alert(JSON.stringify(parseCode(str)));
